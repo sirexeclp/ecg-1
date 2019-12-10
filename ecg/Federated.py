d07f3966eba36b21056d233de1d0e175433a8266
felix.grzelka <felix.grzelka@student.hpi.de>
2019-12-10 11:09:08 +0000

    first try on federated learning

---
+++ ecg/Federated.py
@@ -0,0 +1,71 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.4'
+#       jupytext_version: 1.2.4
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+import keras as keras
+import tensorflow as tf
+
+data_json, model_path = "../examples/cinc17/dev.json","../saved/cinc17/1575022999-481/0.384-0.890-019-0.301-0.898.hdf5"
+model = keras.models.load_model(model_path)
+
+tf.compat.v1.enable_v2_behavior()
+import tensorflow_federated as tff
+
+# +
+import tensorflow as tf
+tf.compat.v1.enable_v2_behavior()
+import tensorflow_federated as tff
+
+# Load simulation data.
+source, _ = tff.simulation.datasets.emnist.load_data()
+def client_data(n):
+  return source.create_tf_dataset_for_client(source.client_ids[n]).map(
+      lambda e: {
+          'x': tf.reshape(e['pixels'], [-1]),
+          'y': e['label'],
+  }).repeat(10).batch(20)
+
+# Pick a subset of client devices to participate in training.
+
+
+# +
+train_data = [client_data(n) for n in range(3)]
+
+# Grab a single batch of data so that TFF knows what data looks like.
+sample_batch = tf.nest.map_structure(
+    lambda x: x.numpy(), iter(train_data[0]).next())
+
+
+# -
+
+# Wrap a Keras model for use with TFF.
+def model_fn():
+  model = tf.keras.models.Sequential([
+      tf.keras.layers.Dense(10, tf.nn.softmax, input_shape=(784,),
+                            kernel_initializer='zeros')
+  ])
+  model.compile(
+      loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+      optimizer=tf.keras.optimizers.SGD(0.1),
+      metrics=[tf.keras.metrics.SparseCategoricalAccuracy()])
+  return tff.learning.from_compiled_keras_model(model, sample_batch)
+
+
+# Simulate a few rounds of training with the selected client devices.
+trainer = tff.learning.build_federated_averaging_process(model_fn)
+state = trainer.initialize()
+for _ in range(5):
+  state, metrics = trainer.next(state, train_data)
+  print (metrics.loss)
+
+
