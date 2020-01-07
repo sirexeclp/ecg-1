8758763c567933bf8dc0ba48f1b395780ca3cb15
felix.grzelka <felix.grzelka@student.hpi.uni-potsdam.de>
2020-01-07 16:24:36 +0000

    add parallel training argument to cmd parser

--- ecg/train.py
+++ ecg/train.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from __future__ import division
 from __future__ import absolute_import
 
@@ -13,7 +14,7 @@ import network
 import load
 import util
 
-MAX_EPOCHS = 2#100
+MAX_EPOCHS = 100
 
 def make_save_dir(dirname, experiment_name):
     start_time = str(int(time.time())) + '-' + str(random.randrange(1000))
@@ -24,9 +25,9 @@ def make_save_dir(dirname, experiment_name):
 
 def get_filename_for_saving(save_dir):
     return os.path.join(save_dir,
-            "{val_loss:.3f}-{val_accuracy:.3f}-{epoch:03d}-{loss:.3f}-{accuracy:.3f}.hdf5")
+            "{val_loss:.3f}-{val_acc:.3f}-{epoch:03d}-{loss:.3f}-{acc:.3f}.hdf5")
 
-def train(args, params, multi_gpu=False):
+def train(args, params):
 
     print(f"Loading training set...{params['train']}")
     train = load.load_dataset(params['train'])
@@ -62,10 +63,18 @@ def train(args, params, multi_gpu=False):
 
     batch_size = params.get("batch_size", 32)
 
-    if multi_gpu:
+    if args.multi_gpu:
         from tensorflow.keras.utils import multi_gpu_model
-        from tensorflow.keras import backend
-        num_gpus = len(backend.tensorflow_backend._get_available_gpus())
+        #from tensorflow.keras import backend
+        from tensorflow.python.client import device_lib
+
+        avail_gpus = len([x for x in device_lib.list_local_devices() if x.device_type  == "GPU"])
+        if not isinstance(args.multi_gpu, bool):
+            num_gpus = min(args.multi_gpu, avail_gpus)
+        else:
+            num_gpus = avail_gpus
+
+        #num_gpus = len(backend.tensorflow_backend._get_available_gpus())
         batch_size *= num_gpus
         print(f"training on {num_gpus} gpus with batch_size {batch_size}, μ-batch™_size {batch_size//num_gpus}")
         parallel_model = multi_gpu_model(model, gpus=num_gpus)
@@ -97,6 +106,9 @@ if __name__ == '__main__':
     parser.add_argument("config_file", help="path to config file")
     parser.add_argument("--experiment", "-e", help="tag with experiment name",
                         default="default")
+    parser.add_argument("--multi-gpu", "-m", help="enable multi gpu-training",
+            action='store', default=False, const=True, nargs="?", type=int)
     args = parser.parse_args()
     params = json.load(open(args.config_file, 'r'))
+    print(args.multi_gpu)
     train(args, params)
